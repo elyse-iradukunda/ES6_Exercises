d2fc037959c4446a3e7eb2ca0a0707a9dd462325
Irael <ellycreativity8@gmail.com>
2025-08-12 11:46:01 +0200

    all done on first link about this  Exercises on variable decralations using var and let

--- FirstExercis.js
+++ FirstExercis.js
@@ -16,3 +16,21 @@ if (true) {
   let  letX = true;
 }
 assert.throws(() => letX, ReferenceError);
+
+// WHEN using let in a for loop THEN the variable is only "visible" inside this loop
+
+let obj = {x: 1};
+for (let key in obj) {
+    
+}
+assert.throws(() => key, ReferenceError);
+// WHEN embedding a let variable in a block (using curly braces) THEN the variable is not "visible" outside of it
+let objj = {x: 1};
+for (let key in objj) {
+    
+}
+assert.throws(() => key, ReferenceError);
+
+// WHEN declaring a variable with let without a value THEN this variable has the value undefined
+let variable;
+assert.strictEqual(variable, undefined);
