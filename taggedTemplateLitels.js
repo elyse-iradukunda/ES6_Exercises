e9436c5e3ea93f53e1ee77e8d84b2d2e3394206b
Irael <ellycreativity8@gmail.com>
2025-08-27 16:07:02 +0200

    this the exercise I did on tagged template literals

---
+++ taggedTemplateLitels.js
@@ -0,0 +1,8 @@
+let name = 'james'
+let price = 1993
+let names = Greating ` Ellyse iradukunda ${name} and the following items overthere ${price}`;
+console.log(names)
+
+function Greating(string, ...args){
+    return args
+}
