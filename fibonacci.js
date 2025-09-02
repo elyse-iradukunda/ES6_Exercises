7d3cf32436f06ddc3fd5528ac9a7754ca27d4259
Irael <ellycreativity8@gmail.com>
2025-09-02 08:48:43 +0200

    first kata I did to day

---
+++ fibonacci.js
@@ -0,0 +1,17 @@
+function * Naming(limit){
+    let a =0;
+    let b= 1;
+    let counter =0;
+    
+    while(counter < limit){
+        yield a;
+        [a,b]=[b,a+b];
+        counter++;
+    }
+
+}
+
+for( let i of Naming(19)){
+    
+    console.log(i)
+}
