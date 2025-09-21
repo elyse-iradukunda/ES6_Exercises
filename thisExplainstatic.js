bff074cea549e8a418eb8326970e4c1801a5ef1d
Irael <ellycreativity8@gmail.com>
2025-09-21 22:02:39 +0200

    about static class

---
+++ thisExplainstatic.js
@@ -0,0 +1,22 @@
+
+  class  irael{
+      
+      #difference = 29;
+      
+      static hello(){
+          
+    return 'welcome here'
+      }
+      
+      greet(){
+          
+          return 'welcome without making it static'
+      }
+      
+  }
+  
+  const ira = new irael()
+  
+  console.log( ira.greet())
+ 
+  console.log( irael.hello())
